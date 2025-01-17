2ad0c8a67632290d7c0a922bf6d045340d8987a6
ininike <inioluwadenaike@gmail.com>
2025-01-17 20:40:17 +0100

    Update scraper.py
    to make it return a json object instead

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 
 def scraper(keyword):
     url = "https://www.arise.tv/"
@@ -8,11 +9,29 @@ def scraper(keyword):
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         articles = soup.find_all('article')
-        results = []
+        
+        results = {}
         
         for article in articles:
             if keyword in article.text.lower():
-                results.append(article.text.strip())
+                title = article.css.select('h3 a')
+                img_link = article.css.select('.img-link')
+                page_link = article.css.select('h3 a')
+                date = article.css.select('.date')
+                posted_by = article.css.select('a[rel = "author"]')
+                desc = article.css.select('p')
+                
+                #to check whether its an empty array before trying to get the data
+                #else it would return an empty string 
+                # this is to prevent error of tring reference an index from an empty array
+                results = {
+                    'title': (title[0]).text if len(title) > 0 else '',
+                    'img_link': (img_link[0]).get('href') if len(img_link) > 0 else '',
+                    'page_link': (page_link[0]).get('href') if len(page_link) > 0 else '',
+                    'date': (date[0]).text if len(date) > 0 else '',
+                    'posted_by': (posted_by[0]).text if len(posted_by) > 0 else '',
+                    'desc': (desc[0]).text if len(desc) > 0 else ''
+                }
         
         return results
     else:
@@ -23,13 +42,14 @@ def scraper(keyword):
 while(True):
     keyword = (input('Enter your keyword: ')).lower()
     results = scraper(keyword)
+
+    results_json = json.dumps(results)
+    print(results_json)
     
-    for result in results:
-        print('..',result,'..')
-        
     if len(results) == 0:
         print('There are no results that match your search')
         
     retry = input('Press Enter to try again or input q to print ')
     if retry == 'q':
         break
+
