93869ef252b58131f125ae046750ca7e85ed1244
ininike <inioluwadenaike@gmail.com>
2025-01-21 15:26:06 +0100

    Update scraper.py
    made it a class based solution

--- scraper.py
+++ scraper.py
@@ -2,117 +2,95 @@ import multiprocessing
 import requests
 from bs4 import BeautifulSoup
 import json
-import time
 
 
-# to get all articles
-def get_articles():
-    url = "https://www.arise.tv/"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        articles = soup.find_all('article')
-        return articles
-    else:
-        print(f"Failed to retrieve the website. Status code: {response.status_code}")
-        return []
-
-#collect data on articles  that match the keyword 
-def scrape(articles):
-    datas = []
-    for article in articles:
-        if keyword in article.text.lower():
-            title = article.css.select('h3 a')
-            img_link = article.css.select('div a img')
-            page_link = article.css.select('h3 a')
-            date = article.css.select('.date')
-            posted_by = article.css.select('a[rel = "author"]')
-            
-            #to check whether its an empty array before trying to get the data
-            #else it would return an empty string 
-            # this is to prevent error of string reference an index from an empty array
-            #desc for now is just a link to the main page where the desc will be retrieved
-            data = {
-                'title': (title[0]).text if len(title) > 0 else '',
-                'img_link': (img_link[0]).get('src') if len(img_link) > 0 else '',
-                'page_link': (page_link[0]).get('href') if len(page_link) > 0 else '',
-                'date': (date[0]).text if len(date) > 0 else '',
-                'posted_by': (posted_by[0]).text if len(posted_by) > 0 else '',
-                'desc': (page_link[0]).get('href') if len(page_link) > 0 else ''
-            }
-            datas.append(data)
-    return datas
-
-#get_desc process function to get article description to complete the scraped article data 
-# with its corresponding desc and convert it all to json
-def get_desc(results_before_desc, i,q):
-        x = results_before_desc[i]
-        url = x['desc']
-        response = requests.get(url)
+class Scraper:
+    def __init__(self, home_url, file_name ):
+        self.home_url = home_url
+        self.file_name = file_name
+    
+    def _get_string(self, soup, selector, type):
+        if type == 'text':
+            return ((soup.css.select(selector))[0]).text 
+        if type == 'link':
+            return ((soup.css.select(selector))[0]).get('href')
+        if type == 'img':
+            return ((soup.css.select(selector))[0]).get('src')    
+        
+    def _get_articles_links(self, link_selector, keyword):
+        response = requests.get(self.home_url)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            desc = soup.css.select('header p p')
-            x['desc'] = (desc[0]).text if len(desc) > 0 else ''
-            x_json = json.dumps(x)
-            print(x_json)
-            q.put(x_json)
+            articles = soup.find_all('article')
+            links = []
+            for article in articles:
+                if keyword.lower() in article.text.lower():
+                    link = self._get_string(article,link_selector,'link')
+                    links.append(link)
+            return links
         else:
-            print(f"Failed to retrieve the website. Status code: {response.status_code}")
-        return []
-
-#consumer process function to read all the json strings in the queue and append to a list           
-def consumer(q, results_before_desc):
-    results = []
-    for x in range(len(results_before_desc)):
-        results.append(q.get())
-    q.put(results)
+            return f"Failed to retrieve the website. Status code: {response.status_code}"
+        
+    def _scrape_article(self, link, selector_dict, queue):
+        response = requests.get(link)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.content, 'html.parser')
+            data = {
+                'title': self._get_string(soup,selector_dict['title'],'text'),
+                'img_link': self._get_string(soup,selector_dict['img_link'],'img'),
+                'page_link': link,
+                'date': self._get_string(soup,selector_dict['date'],'text'),
+                'posted_by': self._get_string(soup,selector_dict['posted_by'],'text'),
+                'desc': self._get_string(soup,selector_dict['desc'],'text')
+            }
+            data_json = json.dumps(data)
+            queue.put(data_json)
+        else:
+            queue.put(f"Failed to retrieve the website({link}). Status code: {response.status_code}") 
     
+    def _consumer(self, queue, length):
+        results = []
+        for _ in range(length):
+            results.append(queue.get())
+        queue.put(results)
+        
+    def scrape(self, selector_dict, keyword = ''):
+        if __name__ == '__main__':
+            multiprocessing.freeze_support()
+            multiprocessing.set_start_method('spawn')
+            queue = multiprocessing.SimpleQueue()
+            
+            links = self._get_articles_links(selector_dict['page_link'],keyword)
+            if isinstance(links,str):
+                return links
+            
+            c = multiprocessing.Process(target=self._consumer, args=(queue, len(links)))
+            c.start()     
+            for i in range(len(links)):
+                p = multiprocessing.Process(target=self._scrape_article, args=(links[i], selector_dict, queue,))
+                p.start() 
+            for i in range(len(links)): 
+                p.join()        
+            c.join(1.0); c.close() ; p.close()
+            
+            results = queue.get()
+            with open(self.file_name,'a') as f:
+                f.write(str(results))
+            return results
+        
 
-#main codeblock
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
-    multiprocessing.set_start_method('spawn')
-    q = multiprocessing.SimpleQueue()
-    print('Getting articles...')
-    start_time = time.time()
-    articles = get_articles()
-    end_time = time.time()
-    time_elasped = end_time - start_time
-    print(f'took {time_elasped:.2f} seconds')
-    while(True):
-        keyword = (input('Enter your keyword: ')).lower() 
-        start_time = time.time()
-        results_before_desc = scrape(articles)
-        end_time = time.time()
-        time_elasped = end_time - start_time
-        print(f'took {time_elasped:.2f} seconds')
-        start_time = time.time()
-        #starting processes
-        c = multiprocessing.Process(target=consumer, args=(q,results_before_desc,))
-        c.start()     
-        for i in range(len(results_before_desc)):
-            p = multiprocessing.Process(target=get_desc, args=(results_before_desc,i,q,))
-            p.start()  
-        for i in range(len(results_before_desc)): 
-            p.join()        
-        c.join(0.0)
-        
-        #output
-        end_time = time.time()
-        results = q.get()
-        
-        print(f'{len(results)} articles found')
-        time_elasped = end_time - start_time
-        print(f'took {time_elasped:.2f} seconds')
-        
-        if len(results) == 0:
-            print('There are no results that match your search')
-           
-        retry = input('Press Enter to try again or input q to quit ')
-        if retry == 'q':
-            c.close()
-            p.close()
-            quit()
-        q.close()
+arisenews = Scraper('https://www.arise.tv/', 'scrape.txt')     
+
+selector_dict = {
+    'title': 'h1',
+    'img_link': '.wp-post-image',
+    'page_link': '.img-link',
+    'date': '.date',
+    'posted_by': 'a[rel = "author"]',
+    'desc': 'header p p'
+}
+
+results =  arisenews.scrape(selector_dict,'tiktok') 
+if results != None:
+    print(str(results)) 
