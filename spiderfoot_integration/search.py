f6cbca35af90e25e4c7222c29bb523d2b6835775
ininike <inioluwadenaike@gmail.com>
2025-02-06 17:18:05 +0100

    Create search.py

---
+++ spiderfoot_integration/search.py
@@ -0,0 +1,31 @@
+import subprocess
+import json
+import re
+
+def clean_output(output):
+    new_string = output[:-4]
+    return new_string.split(",\r\n")
+
+def extract_links(output):
+    links = []
+    for item_json in output:
+        item = json.loads(item_json)
+        url_property = item["data"]
+        site_name = url_property.split()[0]
+        pattern = r'<SFURL>(https?://[^<]+)</SFURL>'
+        site_url = re.findall(pattern, url_property)
+        if site_url == []:
+            site_url = ["No URL found"]
+        data = {"site_name": site_name, "site_url": site_url[0]}
+        data_json = json.dumps(data)
+        links.append(data_json)
+    return links
+
+def run_command(command):
+    stdout = subprocess.check_output(command, shell=True).decode('utf-8')
+    new_array = clean_output(stdout)
+    links = extract_links(new_array)
+    return links
+
+
+    
