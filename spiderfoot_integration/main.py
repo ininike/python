9ec19e064ac1c55fd3937374c713183951013882
ininike <inioluwadenaike@gmail.com>
2025-02-06 17:17:04 +0100

    Create main.py

---
+++ spiderfoot_integration/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from search import run_command
+class Input(BaseModel):
+    search_keyword: str
+
+app =  FastAPI()
+
+@app.post("/socialmedia", status_code=200, description="Search for all possible social media accounts related to the keyword")
+def search_by_name(input: Input):
+    command = f'python spiderfoot/sf.py -m sfp_accounts -s "{input.search_keyword}"  -q -o json'
+    results = run_command(command)
+    return results
+
+
+    
+    
